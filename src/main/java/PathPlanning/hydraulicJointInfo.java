5a4704d21452bd6ed5cbd914c2d7086c63b86c75
wyj <myrljw@163.com>
2021-01-15 09:09:20 +0800

    update joint Obb

--- src/main/java/PathPlanning/hydraulicJointInfo.java
+++ src/main/java/PathPlanning/hydraulicJointInfo.java
@@ -42,17 +42,18 @@ public class hydraulicJointInfo {
         lamba1=Math.sqrt(Math.pow(o1a,2)+Math.pow(o1b,2)-2*o1a*o1b*Utils.cos(ao1b));
         double do2e=Utils.acos((Math.pow(do2,2)+Math.pow(eo2,2)-Math.pow(de,2))/(2*do2*eo2));
         double eo2g=Utils.acos((Math.pow(eo2,2)+Math.pow(o2g,2)-Math.pow(ge,2))/(2*eo2*o2g));
-        double o1o2b=Utils.acos((Math.pow(o1o2,2)+Math.pow(bo2,2)-Math.pow(o1b,2)/(2*o1o2*bo2));
+        double o1o2b=Utils.acos((Math.pow(o1o2,2)+Math.pow(bo2,2)-Math.pow(o1b,2)/(2*o1o2*bo2)));
         double bo2c=Utils.acos((Math.pow(bo2,2)+Math.pow(co2,2)-Math.pow(o1b,2))/(2*bo2*co2));
         double co2d=360-do2e-eo2g-o1o2b-bo2c-theta2;
         lamba2=Math.sqrt(Math.pow(co2,2)+Math.pow(do2,2)-2*co2*do2*Utils.cos(co2d));
 //        double fo3=?;
 //        double ego2=?
 //        double o2go3=?
-//        double ho3o4?
-        double o3gf=Utils.acos((Math.pow(go3,2)+Math.pow(fg,2)-Math.pow(fo3,2))/(2*go3*fg));
-        double egf=360-ego2-o2go3-o3gf;
-        lamba3=Math.sqrt(Math.pow(ge,2)+Math.pow(fg,2)-2*ge*fg*Utils.cos(egf));
+        double ho3o4=Utils.cos((Math.pow(ho3,2)+Math.pow(o3o4,2)-Math.pow(ho4,2))/(2*o3o4*ho3));
+
+//        double o3gf=Utils.acos((Math.pow(go3,2)+Math.pow(fg,2)-Math.pow(fo3,2))/(2*go3*fg));
+//        double egf=360-ego2-o2go3-o3gf;
+//        lamba3=Math.sqrt(Math.pow(ge,2)+Math.pow(fg,2)-2*ge*fg*Utils.cos(egf));
 
     }
 
